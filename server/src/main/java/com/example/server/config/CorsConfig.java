dbe3d4abbbf2c639dafc9d720459dd729ddaa443
EC2 Default User <ec2-user@ip-172-31-21-40.us-west-1.compute.internal>
2022-06-08 12:07:52 +0900

    cors 수정

--- server/src/main/java/com/example/server/config/CorsConfig.java
+++ server/src/main/java/com/example/server/config/CorsConfig.java
@@ -16,8 +16,9 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedHeader("*");
+       	// config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://localhost:3000", "http://ec2-52-9-214-153.us-west-1.compute.amazonaws.com:80");
+	config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader(JwtProperties.HEADER_STRING); // 노출시킬 헤더 -> 응답 시에 프론트에서 받았을 때 노출될 헤더를 설정
         config.addExposedHeader(JwtProperties.REFRESH_HEADER_STRING); // 노출시킬 헤더 -> 응답 시에 프론트에서 받았을 때 노출될 헤더를 설정
