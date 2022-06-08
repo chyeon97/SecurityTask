ba85abf09af0de26db24216aa4426425c212a9b0
chyeon97 <40657327+chyeon97@users.noreply.github.com>
2022-06-08 21:52:16 +0900

    Update CorsConfig.java

--- server/src/main/java/com/example/server/config/CorsConfig.java
+++ server/src/main/java/com/example/server/config/CorsConfig.java
@@ -17,6 +17,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
        	config.addAllowedOrigin("http://ec2-52-9-214-153.us-west-1.compute.amazonaws.com");
+	config.addAllowedOrigin("http://52.9.214.153"); 
 	config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader(JwtProperties.HEADER_STRING); // 노출시킬 헤더 -> 응답 시에 프론트에서 받았을 때 노출될 헤더를 설정
